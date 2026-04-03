97ad0d36ca603e1dad860be5720cb3e3290057c7
dh4rio <marzchelo1@gmail.com>
2026-04-03 01:34:08 -0500

    settings

--- congress/settings.py
+++ congress/settings.py
@@ -200,42 +200,24 @@ CLOUDINARY_STORAGE = {
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# -------------------------- DEVELOPMENT --------------------------
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('DB_NAME'),
-#         'USER': os.getenv('DB_USER'),
-#         'PASSWORD': os.getenv('DB_PASSWORD'),
-#         'HOST': os.getenv('DB_HOST'),
-#         'PORT': os.getenv('DB_PORT') 
-#     }
-# }
-
-# CORS_ALLOW_ALL_ORIGINS = True
-
-# -------------------------- PRODUCTION --------------------------
+# Database
 
 DATABASES = {
     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
-CORS_ALLOW_ALL_ORIGINS = False
+# CORS and CSRF settings
 
-CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = os.getenv('CORS_ALLOW_ALL_ORIGINS', 'False') == 'True'
 
-CORS_ALLOWED_ORIGINS = [
-    "https://conaea-2026-production.up.railway.app",
-    "https://conaea.com",
-    "https://sisgepo-conaea.netlify.app",
-]
+CORS_ALLOW_CREDENTIALS = os.getenv('CORS_ALLOW_CREDENTIALS', 'False') == 'True'
 
-CSRF_TRUSTED_ORIGINS = [
-    "https://conaea-2026-production.up.railway.app",
-    "https://conaea.com",
-    "https://sisgepo-conaea.netlify.app",
-]
+CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', '').split(',')
+
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
+
+# Security settings
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True
+
+SECURE_SSL_REDIRECT = os.getenv('SECURE_SSL_REDIRECT', 'False') == 'True'
